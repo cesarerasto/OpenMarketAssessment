fa9a7307a64852a6b50bdddc2fe4f4895720efc1
Erasto Vargas <thesmash00@gmail.com>
2020-02-18 20:07:09 -0600

    Updates

---
+++ precache-manifest.003684713082c9f2406a32b366e3ed31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53da29be61f467f70c3f4ba25eb1b24",
+    "url": "/OpenMarketAssessment/index.html"
+  },
+  {
+    "revision": "b91750da5969652ad362",
+    "url": "/OpenMarketAssessment/static/css/main.89bd85ae.chunk.css"
+  },
+  {
+    "revision": "3d9421c272328ba504e9",
+    "url": "/OpenMarketAssessment/static/js/2.1535597a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/OpenMarketAssessment/static/js/2.1535597a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91750da5969652ad362",
+    "url": "/OpenMarketAssessment/static/js/main.05efb9c6.chunk.js"
+  },
+  {
+    "revision": "603fceacf6b6507e697b",
+    "url": "/OpenMarketAssessment/static/js/runtime-main.08c073ee.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/OpenMarketAssessment/static/media/logo.25bf045c.svg"
+  }
+]);
